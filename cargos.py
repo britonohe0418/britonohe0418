e4c21ec928add6e950e78a78ebedad41179acc16
Nohelia Brito <britonohe0418@gmail.com>
2021-06-23 13:23:44 -0500

    Ejercicio 1

---
+++ cargos.py
@@ -0,0 +1,16 @@
+class Cargo:
+    secuencia = 0
+    def __init__(self, des='Sin cargo'):
+        Cargo.secuencia += 1
+        self.codigo = Cargo.secuencia
+        self.descripcion = des
+
+
+if __name__ == '__main__':
+    cargo1 = Cargo()
+    print(cargo1.codigo, cargo1.descripcion)
+    cargo2 = Cargo()
+    cargo2.descripcion='Director'
+    print(cargo2.codigo, cargo2.descripcion)
+    cargo3 = Cargo('Analista')
+    print(cargo3.codigo, cargo3.descripcion)
